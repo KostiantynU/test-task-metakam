b5d0831ca2acbf306c270a0091ee7dc9ced14273
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2024-11-04 23:37:33 +0200

    Add basname

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Theme accentColor="yellow" grayColor="olive">
-      <BrowserRouter>
+      <BrowserRouter basename="test-task-metakam">
         <Provider store={store}>
           <PhotoModalContextProvider>
             <App />
